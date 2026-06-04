40a5d22e462bde4bf5c3d4e9a263b80dd11cc602
Ali Abbas <aliabbas@Ali-Abbas.local>
2026-06-04 11:02:41 +0500

    new code add

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import os
 print("Hello, World!")
 print("current dir: ",os.getcwd())
 print(os.listdir())
+if os.path.exists("test.txt"):
+    with open("test.txt", "r") as f:
+        print(f.read())
